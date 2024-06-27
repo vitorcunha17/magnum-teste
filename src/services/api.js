69b1a5e9aaa7aa1200859843d653970fd6e53566
vitorcunha17 <vitorribeirocunha@hotmail.com>
2024-06-26 23:44:43 -0300

    feat: add vercel magnum-test-api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  // para rodar localmente
+  // baseURL: "http://localhost:3001",
+  // para rodar na vercel
+  baseURL: "https://magnum-teste-api.vercel.app",
 });
 
 export default api;
